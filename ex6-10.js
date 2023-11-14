b4ad24d2ad30c55ce3ed414587f914b724863fa5
kyosukeedo <131092531+kyosukeedo@users.noreply.github.com>
2023-11-14 10:26:24 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){ document.getElementById('logo').src='./image/raichu.jpg'; }
-function imgchange2(){ document.getElementById('logo').src='./image/pikachu.jpg';}
+function imgchange1(){ document.getElementById('logo').src='./image/ゾロアーク.png'; }
+function imgchange2(){ document.getElementById('logo').src='./image/メタモン.png';}
