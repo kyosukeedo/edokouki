a3b6654d7580fe0d7d705888fbdc38ac37b42488
kyosukeedo <131092531+kyosukeedo@users.noreply.github.com>
2023-11-28 10:40:51 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
 function imgchange5(){ document.getElementById('zoro').src='./image/メタモン.png'; }
-function imgchange6(){ document.getElementById(7zoro').src='./image/ゾロアーク.png'; }
+function imgchange6(){ document.getElementById('zoro').src='./image/ゾロアーク.png'; }
