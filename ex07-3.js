aa06a531aec04a848c75dc6aded925ddb3f22bb8
kyosukeedo <131092531+kyosukeedo@users.noreply.github.com>
2023-12-12 09:23:44 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,5 +1,5 @@
 function BMI(){
   var x=document.getElementById('height').value;
   var y=document.getElementById('bodyweight').value;
-  var bmi=y/x^2;
+  var bmi=y/(x*x);
   alert('あなたのBMI値は' + bmi + 'です。');}
